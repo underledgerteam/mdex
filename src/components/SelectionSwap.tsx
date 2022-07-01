a6108cd27fd2cb77bf0c07c4371fb501f9494e32
Siriwat <t.siriwat.2240@gmail.com>
2022-07-01 16:08:07 +0700

    🔨 🚧 add component Selection Swap

---
+++ src/components/SelectionSwap.tsx
@@ -0,0 +1,47 @@
+import { Fragment } from "react";
+import InputCurrency from 'src/components/shared/InputCurrency';
+import InputSelect from 'src/components/shared/InputSelect';
+import Card from 'src/components/shared/Card';
+import { SelectionSwapInterface } from "src/types/SelectionSwapInterface";
+
+const SelectionSwap = ({title}: SelectionSwapInterface): JSX.Element => {
+  return (
+    <Fragment>
+      { title && <h1 className="text-3xl font-semibold pl-5">{title}</h1> }
+      <div className="grid grid-cols-3 gap-4">
+        <Card
+          className="bg-base-100 shadow-xl col-span-3 lg:col-span-1"
+          bodyClassName="grid"
+        >
+          <InputSelect 
+            selectionUpdate={title}
+            keyUpdate="chain"
+            selectLabel="Select Network"
+          />
+        </Card>
+
+        <Card
+          className="bg-base-100 shadow-xl col-span-3 lg:col-span-2"
+          bodyClassName="grid grid-cols-2 gap-4"
+        >
+          <Fragment>
+            <InputSelect 
+              selectionUpdate={title}
+              keyUpdate="token"
+              className="col-span-2 lg:col-span-1"
+              selectLabel="Select a Token"
+            />
+            <InputCurrency 
+              selectionUpdate={title}
+              className="col-span-2 lg:col-span-1"
+              delay={1000}
+              maxCurrency={true}
+            />
+          </Fragment>
+        </Card>
+      </div>
+    </Fragment>
+  );
+};
+
+export default SelectionSwap;
