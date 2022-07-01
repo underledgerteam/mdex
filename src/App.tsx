6e5d32e4f3a23ee333de7e8ae64cbd69e8647041
Siriwat <t.siriwat.2240@gmail.com>
2022-07-01 15:47:29 +0700

    🔨 🚧 add swap page

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,12 @@
-import React from 'react';
-import logo from 'src/logo.svg';
-import 'src/App.css';
-import { TEST } from "src/constants/index";
+import React, { Fragment, useContext, useEffect } from 'react';
 
+import SwapPage from "src/pages/SwapPage";
 function App() {
-  console.log(TEST);
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="min-h-screen min-w-screen flex justify-center items-center">
+      <SwapPage />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/pages/SwapPage.tsx
@@ -0,0 +1,28 @@
+import { Fragment, useContext } from 'react';
+import Card from 'src/components/shared/Card';
+import SelectionSwap from 'src/components/SelectionSwap';
+
+import { SwapContext } from "src/contexts/swap.context";
+import { SwapContextInterface } from "src/types/contexts/swap.context";
+
+const SwapPage = (): JSX.Element => {
+  const { swapSwitch } = useContext(SwapContext) as SwapContextInterface;
+  return(
+    <Card
+      className="glass w-full md:w-2/3"
+      titleClassName="text-4xl mb-5"
+      title="Swap"
+    >
+      <Fragment>
+        <SelectionSwap title="Source" />
+        <div className="flex mx-auto my-5">
+          <button className="btn btn-link hover:no-underline text-5xl" onClick={()=> swapSwitch()}>🔃</button>
+        </div>
+        <SelectionSwap title="Destination" />
+        <button className="btn btn-primary mt-8">Connect Wallet</button>
+      </Fragment>
+    </Card>
+  )
+};
+
+export default SwapPage;
