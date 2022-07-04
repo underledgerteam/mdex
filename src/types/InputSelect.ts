daac131c1b058b2f6a084f8895c92d4160a6d7ab
Siriwat <t.siriwat.2240@gmail.com>
2022-07-04 17:05:48 +0700

    🔨 🚧 Update Types for Option List Network

--- src/types/InputSelect.ts
+++ src/types/InputSelect.ts
@@ -1,6 +1,12 @@
 type KeyUpdateType = "chain" | "value" | "token";
+
+export type listOptionType = {
+  value: string,
+  label: string
+};
 export interface InputSelectInterface {
   className?: string,
+  listOption: listOptionType[],
   keyUpdate: KeyUpdateType,
   selectionUpdate: string,
   defaultValue?: string,
