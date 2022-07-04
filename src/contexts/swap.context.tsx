a72753c8f5e59124588de0d082d3709ac25c3456
Siriwat <t.siriwat.2240@gmail.com>
2022-07-04 16:16:43 +0700

    🔨 🚧 Update Contexts Switch Chain/Add Chain

--- src/contexts/swap.context.tsx
+++ src/contexts/swap.context.tsx
@@ -1,5 +1,5 @@
-import { useState, useEffect, createContext } from "react";
-
+import { useState, useEffect, createContext, useContext } from "react";
+import { Web3Context } from "./web3.context";
 import { SwapContextInterface, SwapProviderInterface, SwapType } from  "src/types/contexts/swap.context"
 const defaultValue: SwapContextInterface = {
   reloadSwitch: false,
@@ -13,11 +13,20 @@ const defaultValue: SwapContextInterface = {
 export const SwapContext = createContext<SwapContextInterface>(defaultValue);
 
 export const SwapProvider = ({ children }: SwapProviderInterface) => {
+  const { walletSwitchChain } = useContext(Web3Context);
   const [swap, setSwap] = useState<SwapType>(defaultValue.swap);
   const [reloadSwitch, setReloadSwitch] = useState<boolean>(defaultValue.reloadSwitch);
 
-  const updateSwap = (objSwap: SwapType) => {
-    setSwap(objSwap);
+  const updateSwap = async(objSwap: SwapType) => {
+    const beforeSwitchSwapObj = swap;
+    try {
+      await walletSwitchChain(Number(objSwap.source.chain));
+      setSwap(objSwap);
+    } catch (error: any) {
+      console.error(error);
+      setSwap(beforeSwitchSwapObj);
+      setReloadSwitch(!reloadSwitch);
+    }
   };
 
   const swapSwitch = () => {
--- src/contexts/web3.context.tsx
+++ src/contexts/web3.context.tsx
@@ -1,15 +1,18 @@
 import { useState, useEffect, createContext } from "react";
 import { ethers } from "ethers";
 
+import { SWAP_CONTRACTS } from "src/utils/constants";
 export interface Web3ContextInterface {
   walletAddress: string;
   isConnected: boolean;
+  walletSwitchChain: (chainId: number) => void,
   handleConnectWallet: Function;
 }
 
 const defaultValue: Web3ContextInterface = {
   walletAddress: "",
   isConnected: false,
+  walletSwitchChain: () => { },
   handleConnectWallet: () => { }
 };
 
@@ -35,7 +38,38 @@ export const Web3Provider: React.FC<React.PropsWithChildren> = ({ children }) =>
       console.error(e);
     }
   };
-
+  const walletAddChain = async (chainId: number) => {
+    try {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      await provider.send("wallet_addEthereumChain", [{
+        chainId: ethers.utils.hexlify(Number(chainId))?.replace("0x0", "0x"),
+        chainName: SWAP_CONTRACTS[chainId].NETWORK_NAME,
+        nativeCurrency: {
+          name: SWAP_CONTRACTS[chainId].NATIVE_CURRENCY!.NAME,
+          symbol: SWAP_CONTRACTS[chainId].NATIVE_CURRENCY!.SYMBOL,
+          decimals: SWAP_CONTRACTS[chainId].NATIVE_CURRENCY!.DECIMALS,
+        },
+        rpcUrls: SWAP_CONTRACTS[chainId].RPC_URLS,
+        blockExplorerUrls: SWAP_CONTRACTS[chainId].BLOCK_EXPLORER_URLS,
+      }]);
+    } catch (error) {
+      throw new Error(`Can't Add ${SWAP_CONTRACTS[chainId].NETWORK_NAME} to Wallet`);
+    }
+  };
+  const walletSwitchChain = async (chainId: number) => {
+    try {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const { chainId: currentChain } = await provider.getNetwork();
+      if(chainId !== currentChain){
+        await provider.send("wallet_switchEthereumChain", [{ chainId: ethers.utils.hexlify(chainId)?.replace("0x0", "0x") }]);
+      }
+    } catch (error: any) {
+      if(error.code === 4902){
+        await walletAddChain(chainId);
+      }
+      throw new Error("Can't Switch Chain");
+    }
+  };
   const handleConnectWallet = async (): Promise<void> => {
     try {
       const provider = new ethers.providers.Web3Provider(ethereum);
@@ -58,6 +92,7 @@ export const Web3Provider: React.FC<React.PropsWithChildren> = ({ children }) =>
     <Web3Context.Provider value={{
       walletAddress,
       isConnected,
+      walletSwitchChain,
       handleConnectWallet
     }}>
       {children}
