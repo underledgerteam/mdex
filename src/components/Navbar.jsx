db1e35bda36e6ca9e1ebffaa18c581ba8764cad5
Siriwat <t.siriwat.2240@gmail.com>
2022-07-04 12:05:43 +0700

    🔨 🚧 Fix Invalid DOM property

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,14 +20,14 @@ const Navbar = () => {
       <div className="navbar bg-custom-navbar w-full lg:mx-auto justify-between items-center font-bold lg:px-8 py-5">
         <div className="navbar-start">
           <div className="md:hidden flex dropdown">
-            <label tabindex="0" className="btn btn-white mr-4">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
+            <label tabIndex="0" className="btn btn-white mr-4">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
             </label>
-            <ul tabindex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-              {navData.map((item) => {
+            <ul tabIndex="0" className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+              {navData.map((item, key) => {
                 return (
 
-                  <li><a className="uppercase font-bold" href={item.href}>{item.name}</a></li>
+                  <li key={key}><a className="uppercase font-bold" href={item.href}>{item.name}</a></li>
 
                 );
               })}
@@ -38,10 +38,10 @@ const Navbar = () => {
           </div>
         </div>
         <div className="hidden lg:flex navbar-center"><ul className="menu menu-horizontal p-0">
-          {navData.map((item) => {
+          {navData.map((item, key) => {
             return (
 
-              <li><a href={item.href} className="text-xl flex text-center text-xl text-white uppercase font-bold cursor-pointer">{item.name}</a></li>
+              <li key={key}><a href={item.href} className="text-xl flex text-center text-xl text-white uppercase font-bold cursor-pointer">{item.name}</a></li>
 
             );
           })}
