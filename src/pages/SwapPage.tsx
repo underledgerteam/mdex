f8416c33ed50bde58bac37cda3dda3de45aa1855
Siriwat <t.siriwat.2240@gmail.com>
2022-07-05 14:22:02 +0700

    🔨 🚧 Update list Network and Disabled Btn Swap

--- src/pages/SwapPage.tsx
+++ src/pages/SwapPage.tsx
@@ -1,15 +1,28 @@
-import { Fragment, useContext } from 'react';
+import { Fragment, useContext, useEffect, useState } from 'react';
 import Card from 'src/components/shared/Card';
 import SelectionSwap from 'src/components/SelectionSwap';
 
 import { SwapContext } from "src/contexts/swap.context";
 import { Web3Context } from "src/contexts/web3.context";
 
+import { SWAP_CONTRACTS } from "src/utils/constants";
 
 const SwapPage = (): JSX.Element => {
-  const { swapSwitch } = useContext(SwapContext);
+  const { swap, swapSwitch } = useContext(SwapContext);
   const { walletAddress, isConnected, handleConnectWallet } = useContext(Web3Context);
 
+  const [isSwapDisable, setIsSwapDisable] = useState(false);
+
+  const listOptionNetwork = Object.keys(SWAP_CONTRACTS).map((key) => {
+    return { value: key, label: (<Fragment><img className="mask mask-squircle mr-1" src="https://placeimg.com/160/160/arch" width={30} /> {SWAP_CONTRACTS[Number(key)].NETWORK_SHORT_NAME}</Fragment>) };
+  });
+
+  useEffect(()=>{
+    const checkSwapSourceUndefined = Object.values(swap.source).every((value)=>{ return value !== undefined? true: false });
+    const checkSwapDestinationUndefined = Object.values(swap.destination).every((value)=>{ return value !== undefined? true: false });
+    setIsSwapDisable(!(checkSwapSourceUndefined && checkSwapDestinationUndefined));
+  },[swap]);
+  
   return (
     <Card
       className="glass w-full md:w-2/3"
@@ -17,15 +30,15 @@ const SwapPage = (): JSX.Element => {
       title="Swap"
     >
       <Fragment>
-        <SelectionSwap title="Source" maxCurrency={true} />
+        <SelectionSwap title="Source" maxCurrency={true} listOptionNetwork={listOptionNetwork} />
         <div className="flex mx-auto my-5">
           <button className="btn btn-link hover:no-underline text-5xl" onClick={() => swapSwitch()}>🔃</button>
         </div>
-        <SelectionSwap title="Destination" />
+        <SelectionSwap title="Destination" listOptionNetwork={listOptionNetwork} />
         {!isConnected ? (
-          <button className="btn btn-primary mt-8" onClick={() => handleConnectWallet}>Connect Wallet</button>
+          <button className="btn btn-primary mt-8" onClick={() => handleConnectWallet()}>Connect Wallet</button>
         ) : (
-          <button className="btn btn-primary mt-8">Swap</button>
+          <button className="btn btn-primary mt-8" disabled={isSwapDisable}>Swap</button>
         )}
       </Fragment>
     </Card>
