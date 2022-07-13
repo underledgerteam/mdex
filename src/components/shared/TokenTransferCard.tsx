667e32424322e770ab1292666451369ac1b4586a
Mob <tammanoon24@gmail.com>
2022-07-13 12:08:45 +0700

    🔨 🚧 #46 Update token card
    responsive

--- src/components/shared/TokenTransferCard.tsx
+++ src/components/shared/TokenTransferCard.tsx
@@ -3,11 +3,25 @@ import { TokenCardInterface } from "src/types/TransferRateCollapse";
 const TokenTransferCard = (props: TokenCardInterface) => {
   const { chainName, networkName, imageSrc, value, currencySymbol } = props;
   return (
-    <div className="card card-side">
+    <div className="card card-compact md:card-side">
+
+      <div className="p-2 my-4 md:hidden">
+        <h1 className="card-title">
+          <img className="mask mask-squircle mr-1" src={imageSrc} width={20} alt="token" />
+          {chainName}
+        </h1>
+        <div>
+          {networkName}
+        </div>
+        <div>
+          {`${value} ${currencySymbol}`}
+        </div>
+      </div>
+
       <figure>
-        <img className="mask mask-squircle mr-1" src={imageSrc} width={30} alt="token" />
+        <img className="hidden md:block mask mask-squircle mr-1" src={imageSrc} width={30} alt="token" />
       </figure>
-      <div className="card-body">
+      <div className="hidden md:card-body">
         <h1 className="card-title">
           {chainName}
         </h1>
@@ -15,9 +29,10 @@ const TokenTransferCard = (props: TokenCardInterface) => {
           {networkName}
         </div>
       </div>
-      <div className="flex items-center">
+      <div className="hidden md:flex items-center">
         {`${value} ${currencySymbol}`}
       </div>
+
     </div>
   );
 };
