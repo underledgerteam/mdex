993c8c2dcfed14cd933b64c9d5bedfbc631b1c06
Siriwat <t.siriwat.2240@gmail.com>
2022-07-27 16:08:34 +0700

    🔧 Update Convert 10 Decimal

--- src/utils/calculatorCurrency.util.ts
+++ src/utils/calculatorCurrency.util.ts
@@ -1,7 +1,7 @@
 import Decimal from 'decimal.js';
 Decimal.set({ toExpNeg: -11, toExpPos: 999 })
 const toBigNumber = (number: string | number) => {
-  return new Decimal(number);
+  return new Decimal(number).toDP(10, Decimal.ROUND_DOWN);
 };
 
 export {
